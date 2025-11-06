2373cf980c3efc59cc768195a14ae6d0edf0079d
learnstack129 <learnstack129@gmail.com>
2025-11-06 23:40:48 +0530

    Update User.js

--- LearnStackIndia-main/backend/models/User.js
+++ LearnStackIndia-main/backend/models/User.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 // Import Topic model to get the total number of algorithms dynamically
-const Topic = require('./Topic'); // Assuming Topic model is in the same directory
+
 
 // --- Sub-schema for Algorithm Progress ---
 const algorithmProgressSchema = new mongoose.Schema({
@@ -526,6 +526,7 @@ userSchema.methods.updateRank = function () {
 
 // Unlock the next topic if prerequisites are met
 userSchema.methods.unlockNextTopic = async function () {
+    const Topic = require('./Topic');
     console.log(`[User UnlockNext] Checking for ${this.username}...`);
     const currentTopicId = this.learningPath.currentTopic;
     const topicOrder = this.learningPath.topicOrder;
@@ -651,6 +652,7 @@ userSchema.methods.hasAchievement = function (achievementId) {
 
 
 module.exports = mongoose.model('User', userSchema);
+
 
 
 
