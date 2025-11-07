114684f3ca6883b874045ecca8ad8ed2edbf0ea8
learnstack129 <learnstack129@gmail.com>
2025-11-07 11:19:30 +0530

    Update server.js

--- LearnStackIndia-main/backend/server.js
+++ LearnStackIndia-main/backend/server.js
@@ -87,7 +87,7 @@ app.use('/api/leaderboard', require('./routes/leaderboard'));
 app.use('/api/admin', require('./routes/admin')); // Mount admin routes under /api/admin
 app.use('/api/mentor', require('./routes/mentor')); // <-- ADD THIS NEW MENTOR ROUTE
 app.use('/api/test', require('./routes/test'));
-app.use('/api/doubts', require('./routes/doubts'));
+//app.use('/api/doubts', require('./routes/doubts'));
 
 // --- END ADJUSTED Route Imports ---
 
@@ -157,6 +157,7 @@ process.on('uncaughtException', (err) => {
 
 
 module.exports = app; // Export app for potential testing
+
 
 
 
