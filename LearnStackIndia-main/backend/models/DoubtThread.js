874f58b0b7ff1c8a52fb96c1d2788d6176ac99d5
learnstack129 <learnstack129@gmail.com>
2025-11-09 00:33:25 +0530

    Update DoubtThread.js

--- LearnStackIndia-main/backend/models/DoubtThread.js
+++ LearnStackIndia-main/backend/models/DoubtThread.js
@@ -1,6 +1,6 @@
 // backend/models/DoubtThread.js
 const mongoose = require('mongoose');
-
+require('./User');
 const doubtThreadSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
